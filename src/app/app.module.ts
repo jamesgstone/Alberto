21054e81b15d01ab4f1286dfab15b54119b0a2ff
Your NameJGS <you@example.com>
2022-02-08 18:46:24 +0200

    added 2 services (login,reg) for http req (empty)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -16,7 +16,9 @@ import { LoginComponent } from './components/login/login.component';
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		FormsModule
+		FormsModule,
+    HttpClientModule
+
 	],
 	providers: [],
 	bootstrap: [AppComponent]
