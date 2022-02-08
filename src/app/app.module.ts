5e85d55565450504897483ca03dbb432838dfadb
Your NameJGS <you@example.com>
2022-02-08 18:38:16 +0200

    Added login form and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		RegisterComponent
+		RegisterComponent,
+  LoginComponent
 	],
 	imports: [
 		BrowserModule,
